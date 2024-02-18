c91bfd3b2e258bd557e05c2198fd2c683c537edd
unknown <1765796778@qq.com>
2024-02-18 12:22:50 +0800

    test2

--- Z-UI/src/App.vue
+++ Z-UI/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
@@ -13,7 +13,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
