8d47a3c883673a7262224bacdfd83afacd68f213
csdoge007 <1765796778@qq.com>
2024-02-23 19:54:50 +0800

    button第一版

--- Z-UI/src/App.vue
+++ Z-UI/src/App.vue
@@ -1,47 +1,57 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue';
+import Button from './components/Button/Button.vue';
+import type { ButtonInstance } from './components/Button/types';
+const buttonInstance = ref<ButtonInstance | null>();
+onMounted(() => {
+  if (buttonInstance.value) {
+    console.log(buttonInstance.value._ref);
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <h>hello world</h>
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-    </div>
-  </header>
-
+  <!-- <Button ref="buttonInstance">TestButton</Button>
+      
+    <Button ref="buttonRef">Test Button</Button> -->
   <main>
-    <!-- <TheWelcome /> -->
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button>
+    <Button disabled>Disabled</Button><br /><br />
+
+    <Button plain>Plain</Button>
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button>
+    <Button disabled plain>Disabled</Button><br /><br />
+
+    <Button round>Round</Button>
+    <Button type="primary" round>Primary</Button>
+    <Button type="success" round>Success</Button>
+    <Button type="info" round>Info</Button>
+    <Button type="warning" round>Warning</Button>
+    <Button type="danger" round>Danger</Button>
+    <Button disabled round>Disabled</Button><br /><br />
+
+    <Button circle>circle</Button>
+    <Button type="primary" circle>Primary</Button>
+    <Button type="success" circle>Success</Button>
+    <Button type="info" circle>Info</Button>
+    <Button type="warning" circle>Warning</Button>
+    <Button type="danger" circle>Danger</Button>
+    <Button disabled circle>Disabled</Button><br /><br />
+
+
+
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ Z-UI/src/components/Button/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button 
+    ref="_ref"
+    class="vk-button" 
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ButtonProps } from './types';
+defineOptions({
+  name: 'ZkButton'
+})
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+});
+const _ref = ref<HTMLButtonElement>();
+defineExpose({
+  _ref,
+})
+</script>
+
+<style scoped></style>
