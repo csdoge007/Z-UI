bd96a08e25dc796fbf0eb81986c9ef91dfc5b039
csdoge007 <1765796778@qq.com>
2025-01-22 22:20:03 +0800

    feat: 初步完成message组件

--- Z-UI/src/components/Message/Message.vue
+++ Z-UI/src/components/Message/Message.vue
@@ -2,6 +2,7 @@
   <div
     class="vk-message"
     role="alert"
+    v-if="visible"
     :class="{
       [`vk-message--${type}`]: type,
       'is-close': showClose
@@ -13,18 +14,39 @@
       </slot>
     </div>
     <div class="vk-message__close" v-if="showClose">
+      <Icon icon="xmark" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import type { MessageProps } from './types'
 import RenderVnode from '../Common/RenderVnode'
+import Icon from '../Icon/Icon.vue'
 const props = withDefaults(defineProps<MessageProps>(), {
   type: 'info',
   duration: 2800
 })
+const visible = ref(true)
+const startCloseTimer = () => {
+  if (props.duration <= 0) return
+  setTimeout(() => {
+    visible.value = false
+  }, props.duration)
+}
+onMounted(() => {
+  startCloseTimer()
+})
 </script>
 
 <style scoped>
+.vk-message {
+  width: max-content;
+  position: fixed;
+  left: 50%;
+  top: 20%;
+  transform: translateX(-50%);
+  border: 1px solid blue;
+}
 </style>
